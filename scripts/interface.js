89498254537b787ef62aa31899c641cce9226a43
Leonardo <leonardopenz.ti@gmail.com>
2022-12-01 06:30:29 -0300

    realizada a lógica do game

--- scripts/interface.js
+++ scripts/interface.js
@@ -8,3 +8,26 @@ document.addEventListener('DOMContentLoaded', ()=>{
     })
 
 })
+
+function handleClick(event){
+    let square = event.target 
+    let position = square.id
+
+    handleMove(position)
+    updateSquares()
+}
+
+function updateSquares(){
+
+    let squares = document.querySelectorAll(".square")
+
+    squares.forEach((square)=>{
+        let position = square.id
+        let symbol = board[position]
+
+        if (symbol != ''){
+            square.innerHTML = `<div class= '${symbol}'></div>`
+        }
+    })
+
+}
